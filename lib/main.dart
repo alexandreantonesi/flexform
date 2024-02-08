4d0d664fe8a081df90ee90b0388e1f1066fa9341
Alexandre Antonesi <ti212188@ensiguarda.com>
2024-02-08 12:52:34 +0000

    inicio de desenvolvimento do screen de setup inicial

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: UserSetupScreen(),
-      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),\
     );
   }
 }
--- lib/screens/user_setup_screen.dart
+++ lib/screens/user_setup_screen.dart
@@ -18,3 +18,95 @@ class _UserSetupScreen extends State<UserSetupScreen> {
     );
   }
 }
+
+class SelectionData {
+  int? daysAvailable;
+  int? hoursAvailable;
+  int? mainGoal;
+
+  SelectionData({this.daysAvailable, this.hoursAvailable, this.mainGoal});
+}
+
+class SetupProcess extends StatefulWidget {
+  @override
+  _SetupProcessState createState() => _SetupProcessState();
+}
+
+class _SetupProcessState extends State<SetupProcess> {
+  int currentStep = 0;
+  SelectionData data = SelectionData();
+
+  void nextStep() {
+    //definição do próximo passo
+    if(currentStep < 2) {
+      setState(() {
+        currentStep++;
+      });
+    //finalizar o setup
+    } else {
+      
+    }
+  }
+
+  void previousStep() {
+    if(currentStep > 0) {
+      setState(() {
+        currentStep--;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> stepWidgets = [
+      StepOne(onSelection: (value) => data.daysAvailable = value),
+      StepTwo(onSelection: (value) => data.hoursAvailable = value),
+      StepTwo(onSelection: (value) => data.mainGoal = value),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Processo de Setup"),
+      ),
+      body: stepWidgets[currentStep],
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          if(currentStep > 0)
+            FloatingActionButton(
+              onPressed: previousStep,
+              child: Icon(Icons.arrow_back)
+            ),
+          FloatingActionButton(
+            onPressed: nextStep,
+            child: Icon(Icons.arrow_forward),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//passo 1
+class StepOne extends StatefulWidget {
+  final ValueChanged<int> onSelection;
+
+  StepOne({Key? key, required this.onSelection});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container()
+  }
+}
+
+class StepTwo extends StatelessWidget {
+  final Function(int) onSelection;
+
+  StepTwo({required this.onSelection});
+}
+
+class StepThree extends StatelessWidget {
+  final Function(int) onSelection;
+
+  StepThree({required this.onSelection});
+}
