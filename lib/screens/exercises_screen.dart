631414f3241660e4a01e0a86674b466b398303d1
Alexandre Antonesi <ti212188@ensiguarda.com>
2024-03-02 18:02:31 +0000

    alt

--- lib/screens/exercises_screen.dart
+++ lib/screens/exercises_screen.dart
@@ -1,28 +1,26 @@
+import 'package:flexform/models/selection_data.dart';
 import 'package:flutter/material.dart';
 
 class ExercisesScreen extends StatelessWidget {
-  // Assuming that userPreferences should be of type Map, add the type here.
-  // If the type is different, replace 'Map' with the correct type.
-  final Map userPreferences;
+  final SelectionData userPreferences;
 
-  // If the ExercisesScreen constructor is not supposed to be const, remove the const keyword.
   ExercisesScreen({required this.userPreferences});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Weekly Exercise Plan'),
+        title: const Text('Plano Semanal de Exercícios'),
       ),
       body: ListView(
         children: <Widget>[
-          ListTile(title: const Text('Monday: Push exercises')),
-          ListTile(title: const Text('Tuesday: Pull exercises')),
-          ListTile(title: const Text('Wednesday: Rest day')),
-          ListTile(title: const Text('Thursday: Push exercises')),
-          ListTile(title: const Text('Friday: Pull exercises')),
-          ListTile(title: const Text('Saturday: Legs exercises')),
-          ListTile(title: const Text('Sunday: Rest day')),
+          ListTile(title: const Text('Segunda-feira: Exercícios de Empurrar')),
+          ListTile(title: const Text('Terça-feira: Exercícios de Puxar')),
+          ListTile(title: const Text('Quarta-feira: Dia de Descanso')),
+          ListTile(title: const Text('Quinta-feira: Exercícios de Empurrar')),
+          ListTile(title: const Text('Sexta-feira: Exercícios de Puxar')),
+          ListTile(title: const Text('Sábado: Exercícios de Pernas')),
+          ListTile(title: const Text('Domingo: Dia de Descanso')),
         ],
       ),
     );
